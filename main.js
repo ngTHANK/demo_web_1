5dc73b0445e625339f159e916baeca9e1b208f60
thanknef <93968586+ngTHANK@users.noreply.github.com>
2022-06-23 09:28:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Hi WELCOME TO NGUYỄN VIẾT TÀI INTRODUCTION PAGE');
+
+
+var check = prompt("Xac minh mat khau!!!");
+if (check == 2003) {
+    alert('continue');
+    confirm('tiep tuc nao');
+} else {
+    alert("cut'");
+}
